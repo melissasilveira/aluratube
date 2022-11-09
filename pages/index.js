d2c21dc2ae6bb7823bfeec75b1faffba8e9f679a
melissasilveira <melissaasilveira@gmail.com>
2022-11-09 19:00:49 -0300

    atualização do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import config from '../config.json'
 import styled from 'styled-components'
-import { CSSReset } from '../src/components/CSSReset'
 import Menu from '../src/components/Menu'
 import { StyledTimeline } from '../src/components/Timeline'
 import React from 'react'
@@ -11,7 +10,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div
         style={{
           display: 'flex',
@@ -33,6 +31,7 @@ function HomePage() {
 }
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
   img {
     width: 80px;
     height: 80px;
